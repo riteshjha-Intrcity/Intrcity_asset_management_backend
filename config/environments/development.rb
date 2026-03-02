8010ee0eee4ea1f9fb4f54cad73a702bfaa8592d
Ritesh <jha61253@gmail.com>
2026-03-02 05:48:53 +0000

    Now added credential in env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "localhost",
-    user_name: "jha43547@gmail.com",
-    password: "wrbulggkihqjlrvv",
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true
   }
